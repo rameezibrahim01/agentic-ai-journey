51a26fe9156cb2d161537fa39ddc8357b3752f03
Rameez Ibrahim <Rameez.ahmed@presight.ai>
2026-05-19 16:26:33 +0400

    minor changes

--- phase-0-python/week-2/day12_practice.py
+++ phase-0-python/week-2/day12_practice.py
@@ -1,30 +1,30 @@
-# print("•	Safe int input: write a function safe_int_input(prompt) that keeps asking until user enters a valid integer.")
+print("•	Safe int input: write a function safe_int_input(prompt) that keeps asking until user enters a valid integer.")
 
-# def safe_int_input(prompt):
-#     try:
-#         value = int(input(f"{prompt}: "))
-#         print(f"You have entered: {value}")
-#     except ValueError:
-#         print("Not an integer")
-#         safe_int_input(prompt)
+def safe_int_input(prompt):
+    try:
+        value = int(input(f"{prompt}: "))
+        print(f"You have entered: {value}")
+    except ValueError:
+        print("Not an integer")
+        safe_int_input(prompt)
 
-# safe_int_input("Your age?")
+safe_int_input("Your age?")
 
-# print("•	Safe file reader: write read_json_safe(filename) that returns the dict, or an empty dict if file doesn't exist OR is invalid JSON.")
+print("•	Safe file reader: write read_json_safe(filename) that returns the dict, or an empty dict if file doesn't exist OR is invalid JSON.")
 
-# import json
+import json
 
-# def read_json_safe(filename):
-#     try:
-#         with open(filename, "r") as f:
-#             return json.load(f)
-#     except FileNotFoundError:
-#         return {}
-#     except json.JSONDecodeError:
-#         return {}
+def read_json_safe(filename):
+    try:
+        with open(filename, "r") as f:
+            return json.load(f)
+    except FileNotFoundError:
+        return {}
+    except json.JSONDecodeError:
+        return {}
     
-# print(read_json_safe("random.py"))
-# print(read_json_safe("phase-0-python/week-2/output-files/grades.json"))
+print(read_json_safe("random.py"))
+print(read_json_safe("phase-0-python/week-2/output-files/grades.json"))
 
 print("•	Calculator with error handling: prompt for two numbers and an operator (+, -, *, /). Handle ValueError (bad number) and ZeroDivisionError.")
 
